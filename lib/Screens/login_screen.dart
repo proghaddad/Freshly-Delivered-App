2f10e8009556b4804124c8fe438743d7909d33be
Yılmaz Yağız <dokumaciyagiz@gmail.com>
2022-01-29 15:13:07 +0300

    Background color update on login screem

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -19,44 +19,40 @@ class _LoginScreenState extends State<LoginScreen> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
-        width: size.width / 1,
-        height: size.height / 1,
-        color: HexColor('#25C573'),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 60),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset('lib/Assets/img/freshlogo.png',
-                    height: size.height / 6.5),
-                const Padding(
-                  padding: EdgeInsets.only(top: 25.0),
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  ),
+      backgroundColor: HexColor('#25C573'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 60),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('lib/Assets/img/freshlogo.png',
+                  height: size.height / 6.5),
+              const Padding(
+                padding: EdgeInsets.only(top: 25.0),
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
                 ),
-                LoginTextField(
-                  obsecure: false,
-                  controller: nameController,
-                  preficon: Icons.email,
-                  sufficon: Icon(Icons.remove_red_eye,color: HexColor('#fff'),),
-          ),
-                LoginTextField(
-                  obsecure: true,
-                  controller: passController,
-                  preficon: Icons.lock,
-                  sufficon: Icon(Icons.remove_red_eye,color: HexColor('#48742C'),),
-                ),
-                ForgotText(),
-                LoginBTN(),
-                AccountTXT(),
-              ],
-            ),
+              ),
+              LoginTextField(
+                obsecure: false,
+                controller: nameController,
+                preficon: Icons.email,
+                sufficon: Icon(Icons.remove_red_eye,color: HexColor('#fff'),),
+        ),
+              LoginTextField(
+                obsecure: true,
+                controller: passController,
+                preficon: Icons.lock,
+                sufficon: Icon(Icons.remove_red_eye,color: HexColor('#48742C'),),
+              ),
+              ForgotText(),
+              LoginBTN(),
+              AccountTXT(),
+            ],
           ),
         ),
       ),
